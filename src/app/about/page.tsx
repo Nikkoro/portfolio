759c062ca1756b1e16f4d114e186bbfb27909a4c
nikkoro <nikodemdomaracki@gmail.com>
2023-11-21 19:56:59 +0100

    feat: :sparkles: Added empty pages for temporary routing

---
+++ src/app/about/page.tsx
@@ -0,0 +1,3 @@
+export default function About() {
+  return <main className=" p-12 pt-0 lg:p-24 lg:pt-0">About</main>;
+}
---
+++ src/app/projects/page.tsx
@@ -0,0 +1,3 @@
+export default function Projects() {
+  return <main className=" p-12 pt-0 lg:p-24 lg:pt-0">Projects</main>;
+}
