9fd4ad6780cfcffbb4a90433597cd779e8530519
nikkoro <nikodemdomaracki@gmail.com>
2023-11-21 19:50:34 +0100

    feat: :lipstick: Moved welcome to body

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,11 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main className=" p-12 pt-0 lg:p-24 lg:pt-0">
+    <main className=" p-14 pt-0 lg:p-24 lg:pt-0">
       <section className="flex flex-col gap-8 py-24 pt-0 sm:justify-items-start ">
+        <p className="text-3xl font-bold lg:text-5xl ">
+          Hello! <span className="inline-block hover:animate-wave ">👋</span>
+        </p>
         <h1 className="text-2xl lg:text-3xl">
           Hi, my name is{" "}
           <code className="font-mono text-3xl font-bold lg:text-4xl">
