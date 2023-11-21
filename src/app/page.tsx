d5ea056510afb01705945e08468dc244ff84787e
nikkoro <nikodemdomaracki@gmail.com>
2023-11-22 00:48:39 +0100

    style: :lipstick: Applied breakpoints for bigger screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,13 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main className=" p-14 pt-0 lg:p-24 lg:pt-0">
+    <main className=" 4xl:px-96 p-14 pt-0 lg:p-24 lg:pt-0">
       <section className="flex flex-col gap-8 py-24 pt-0 sm:justify-items-start ">
         <p className="text-3xl font-bold lg:text-5xl ">
-          Hello! <span className="inline-block hover:animate-wave ">👋</span>
+          Hello!{" "}
+          <span className="sm:data-te-animation inline-block hover:animate-wave">
+            👋
+          </span>
         </p>
         <h1 className="text-2xl lg:text-3xl">
           Hi, my name is{" "}
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -17,7 +17,7 @@ import Image from "next/image";
 
 export default function Nav() {
   return (
-    <header className="p-12 lg:px-24">
+    <header className="4xl:px-96 p-12 lg:px-24">
       <nav>
         <ul className="flex items-center justify-between  ">
           <Image
