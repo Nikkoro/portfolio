e67164e5302b15c35fef844fa3671c56584cee74
nikkoro <nikodemdomaracki@gmail.com>
2023-11-21 01:38:25 +0100

    style: :lipstick: Changed navbar text

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,12 +1,16 @@
 "use client";
 
+import { ModeToggle } from "./ui/theme-toggle";
+
 export default function Nav() {
   return (
     <header>
       <nav>
         <ul className="flex items-center justify-between">
-          <li className="text-3xl font-bold">nkayo</li>
-          <li>Toggle darkmode</li>
+          <li className="text-3xl font-bold">Hello! 👋</li>
+          <li>
+            <ModeToggle />
+          </li>
         </ul>
       </nav>
     </header>
