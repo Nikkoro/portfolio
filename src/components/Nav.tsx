505bee31bcffa85c14bd093885e8267cdb27f8f2
nikkoro <nikodemdomaracki@gmail.com>
2023-11-21 01:17:31 +0100

    feat: :lipstick: Created simple navbar
    Navbar placeholder for now

---
+++ src/components/Nav.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+export default function Nav() {
+  return (
+    <header>
+      <nav>
+        <ul className="flex items-center justify-between">
+          <li className="text-3xl font-bold">nkayo</li>
+          <li>Toggle darkmode</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
