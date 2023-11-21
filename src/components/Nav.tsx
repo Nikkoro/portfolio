ce4413dbc8f8a68ebde99c8372555f3fe8b00317
nikkoro <nikodemdomaracki@gmail.com>
2023-11-21 14:06:00 +0100

    feat: :lipstick: Animated emoji to "wave"

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -6,8 +6,10 @@ export default function Nav() {
   return (
     <header>
       <nav>
-        <ul className="flex items-center justify-between">
-          <li className="text-3xl font-bold">Hello! 👋</li>
+        <ul className="flex items-center justify-between ">
+          <li className="text-5xl font-bold ">
+            Hello! <span className="hover:animate-wave inline-block ">👋</span>
+          </li>
           <li>
             <ModeToggle />
           </li>
