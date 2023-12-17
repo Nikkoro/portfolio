2291b2c89c59d9d971b7eccd119dfd8a496a3e3c
nikkoro <nikodemdomaracki@gmail.com>
2023-12-17 23:37:31 +0100

    feat: :sparkles: render our skills component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Projects from "@/components/Projects";
 import About from "@/components/About";
+import Skills from "@/components/Skills";
 
 export default function Home() {
   return (
     <main className=" container relative flex min-h-screen flex-col ">
       <About />
       <Projects />
+      <Skills />
     </main>
   );
 }
