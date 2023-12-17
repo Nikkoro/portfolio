3576efbc0e39d9ce42b8f91a48a4b5be699f6678
nikkoro <nikodemdomaracki@gmail.com>
2023-12-17 23:22:27 +0100

    feat: :sparkles: New component for displaying known technologies

---
+++ src/components/Skills.tsx
@@ -0,0 +1,42 @@
+"use client";
+import React from "react";
+import { skills } from "./data";
+import { Badge } from "./ui/badge";
+import { Card, CardContent } from "./ui/card";
+import { IconContext } from "react-icons";
+
+export default function Skills() {
+  return (
+    <IconContext.Provider
+      value={{
+        size: "32",
+        style: { strokeWidth: 1.2 },
+      }}
+    >
+      <section className="w-full py-8  md:py-12" id="skills">
+        <div className="container grid items-center justify-center gap-4 px-0 text-center  lg:gap-10">
+          <div className="space-y-3">
+            <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
+              Skills dupa
+            </h2>
+          </div>
+          <Card>
+            <CardContent className="flex flex-wrap p-2">
+              {skills.map((skill) => (
+                <Badge
+                  variant={"secondary"}
+                  className="m-2 items-center text-sm lg:text-lg"
+                  key={skill.name}
+                >
+                  {skill.icon}
+                  &nbsp;
+                  {skill.name}
+                </Badge>
+              ))}
+            </CardContent>
+          </Card>
+        </div>
+      </section>
+    </IconContext.Provider>
+  );
+}
