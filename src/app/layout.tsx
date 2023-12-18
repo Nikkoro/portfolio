6199fbc3895c61ec15e5c04884391c2502e1b226
nikkoro <nikodemdomaracki@gmail.com>
2023-12-18 03:43:40 +0100

    feat: :lipstick: New footer component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/toaster";
 import Nav from "@/components/Nav";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -34,6 +35,7 @@ export default function RootLayout({
           <Nav />
           {children}
           <Toaster />
+          <Footer />
         </ThemeProvider>
       </body>
     </html>
---
+++ src/components/Footer.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { GitHubLogoIcon, LinkedInLogoIcon } from "@radix-ui/react-icons";
+import { useTheme } from "next-themes";
+
+export default function Footer() {
+  //TODO: find a better way to handle this
+  function handleThemeChange() {
+    const { resolvedTheme } = useTheme();
+    let message;
+
+    switch (resolvedTheme) {
+      case "light":
+        message = "Made with 🖤 by nikodemdomaracki";
+        break;
+      case "dark":
+        message = "Made with 🤍 by nikodemdomaracki";
+        break;
+      default:
+        message = "Made with ❤️ by nikodemdomaracki";
+        break;
+    }
+    return message;
+  }
+
+  return (
+    <footer className="bottom-0 z-50 w-full border-t bg-background">
+      <div className="container py-2 ">
+        <ul className="flex items-center justify-between">
+          <li>
+            <span>{handleThemeChange()}</span>
+          </li>
+          <li>
+            <a href="https://github.com/Nikkoro" aria-label="Link to my github">
+              <GitHubLogoIcon className="h-8 w-8" />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/nikodemdomaracki/"
+              aria-label="Link to my linkedin"
+            >
+              <LinkedInLogoIcon className="h-8 w-8" />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+}
